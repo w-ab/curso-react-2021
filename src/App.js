9570313738b9f36954b536a6bfa90cd8230493c7
w-ab <ookamiookami42@gmail.com>
2021-12-03 21:52:33 -0300

    Resultado até a aula 13

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+function ComponenteFuncional(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1>Hello</h1>
+  )
+}
+
+class App extends React.Component {
+  state = {
+    nome : ''
+  }
+
+  tituloBacana = () => {
+    return (
+      <h1>Titulo legal</h1>
+    )
+  }
+
+  modificarNome = (event) => {
+    // nao funciona acessar direto
+    // this.state.nome = outro_nome
+    let outro_nome = event.target.value;
+    this.setState({
+      nome: outro_nome
+    })
+  }
+
+  constructor() {
+    super()
+    this.criaComboBox = this.criaComboBox.bind(this)
+  }
+  // pracisa do contrutor acima por nao ser uma arrow function
+  // caso contrario o this nao referenciaria a classe e sim a funcao
+  // o que gera erro
+  criaComboBox(){
+    const opcoes = [ "Fresco", "Curado" ]
+    const comboBoxOpcoes = opcoes.map( opcao => <option>{opcao}</option> )
+    //opcoes.map( opcao => return {(<option>{opcao}</option>)} )
+    
+    return (
+      <select>
+        {comboBoxOpcoes}
+      </select>
+    )
+  }
+
+  render(){
+    const MeuTitulo = () => this.tituloBacana()
+    
+    return (
+      <React.Fragment>
+        <MeuTitulo />
+        <input type="text" value={this.state.nome} onChange={this.modificarNome}/>
+        <h1>Olá {this.state.nome}, você tem {this.props.idade} anos!</h1>
+        {this.criaComboBox()}
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
